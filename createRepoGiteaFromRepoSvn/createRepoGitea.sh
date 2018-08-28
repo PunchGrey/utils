772a8943aa05fb2b7fe9c867d4bc9a17b76f96db
PunchGrey <GalSergGrey@yandex.ru>
2018-08-28 16:53:48 +0300

    Добавил возможность второй группы, утилита snaketoCamel теперь работает и с "-" (test-test -> testTest)

--- createRepoGiteaFromRepoSvn/createRepoGitea.sh
+++ createRepoGiteaFromRepoSvn/createRepoGitea.sh
@@ -13,6 +13,7 @@ nameOrg="test"
 svnUrl="svn://your_url"
 gitUrl="http://git.yourdomain.name"
 idTeam = "11"
+idTeam2 = ""
 
 ./snakeToCamel -in $fileIn -out $fileOut -pref "${prefix}"
 
@@ -25,7 +26,14 @@ do
     nameRepo=${nameRepo[1]}
     
     curl -X POST "${gitUrl}/api/v1/org/${nameOrg}/repos?access_token=${token}" -H "accept: application/json" -H "content-type: application/json" -d "{\"name\":\"${nameRepo}\", \"description\": \"${description}\" }"
-    curl -X PUT "${gitUrl}/api/v1/teams/${idTeam}/repos/${nameOrg}/${nameRepo}?access_token=${token}" -H "accept: application/json" -H "content-type: application/json"
+    if [ -n "$idTeam" ]   
+    then
+        curl -X PUT "http://git.srv.sec45.ccr.dep4.niitp/api/v1/teams/${idTeam}/repos/${nameOrg}/${nameRepo}?access_token=${token}" -H "accept: application/json" -H "content-type: application/json"
+    fi
+    if [ -n "$idTeam2" ]   
+    then
+        curl -X PUT "http://git.srv.sec45.ccr.dep4.niitp/api/v1/teams/${idTeam2}/repos/${nameOrg}/${nameRepo}?access_token=${token}" -H "accept: application/json" -H "content-type: application/json"
+    fi
 
     git svn clone --authors-file=$fileUsers --username $userName --no-metadata --stdlayout ${svnUrl}/${nameDir} $nameDir  #удалил trunk
     cd $nameDir
